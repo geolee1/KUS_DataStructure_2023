405f0dddca7949c058252e7bb0af7e677fcee27b
Geo Lee <geolee1@naver.com>
2023-11-16 00:32:05 +0900

    create hash search files

---
+++ hash_search/src/main.py
@@ -0,0 +1,28 @@
+# 자료구조개론 9주차 과제7
+# hash와 linked list를 이용한 검색 구현
+#
+# 미래모빌리티학과
+# 2021271402 이지오
+
+# <전제 1> 임의의 정수 1만개가 있다.
+# <조건 1> bucket을 97개 만든다. (하필이면 97인가?)
+# <조건 2> 각각의 bucket에 1만개의 정수를 linked list로 위치 시킨다.
+# <조건 3> 임의의 숫자가 몇 번째 bucket의 몇 번째 linked list에 위치하는지 출력
+# 없다면 “not found” 출력
+
+# True : print debug message
+DEBUG = False   
+
+
+# module containing
+import sys      # for call main function
+
+
+# define main function
+def main(*args, **kwargs) -> int:
+    return 0
+
+
+# call main function
+if __name__ == '__main__':
+    sys.exit(main())
