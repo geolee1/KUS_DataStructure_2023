f3fea05efc344b96e8caf04d9d8ec705b05a0ce3
Geo Lee <geolee1@naver.com>
2023-11-16 13:09:47 +0900

    add linked list

--- hash_search/src/main.py
+++ hash_search/src/main.py
@@ -16,6 +16,127 @@ DEBUG = False
 
 # module containing
 import sys      # for call main function
+import random   # for create random data
+
+
+# Linked List
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+     
+class LinkedList:
+    def __init__(self, iterable_data = None):
+        self.clear()
+        if iterable_data:
+            self.extend(iterable_data)
+            
+    def __str__(self) -> str:
+        pass
+    
+    def __repr__(self) -> str:
+        pass
+    
+    def __len__(self) -> int:
+        return self._len
+    
+    def __iter__(self):
+        pass
+    
+    def __next__(self):
+        pass
+    
+    # Add an item to the end of the linked list.
+    def append(self, data):
+        new_node = Node(data)
+        if self._head is None:
+            self._head = new_node
+            self._tail = new_node
+            self._len += 1
+        else:
+            self._tail.next = new_node
+            self._tail = new_node
+            self._len += 1
+            
+    # Extend the linked list by appending all the items from the iterable.
+    def extend(self, iterable_data):
+        for data in iterable_data:
+            self.append(data)
+    
+    # Insert an item at a given position. 
+    # The first argument is the index of the element before which to insert, 
+    # so a.insert(0, x) inserts at the front of the linked list, 
+    # and a.insert(len(a), x) is equivalent to a.append(x).
+    def insert(self, index, data):
+        new_node = Node(data)
+        if index == 0:
+            new_node.next = self._head
+            self._head = new_node
+            self._len += 1
+        else:
+            node = self._head
+            for _ in range(index - 1):
+                node = node.next
+            new_node.next = node.next
+            node.next = new_node
+            self._len += 1
+    
+    # Remove the first item from the list whose value is equal to x. 
+    # It raises a ValueError if there is no such item.
+    def remove(self, data):
+        pass
+    #     if self._head.data == data:
+    #         self._head = self._head.next
+    #         self._count -= 1
+    #     else:
+    #         node = self._head
+    #         while node.next.data != data:
+    #             node = node.next
+    #         if node.data != data:
+    #             raise ValueError(f"{data} is not in linked list.")
+    #         if node.next == None:
+    #         node.next = node.next.next
+    #         self._count -= 1
+    
+    # Remove the item at the given position in the list, and return it. 
+    # If no index is specified, a.pop() removes and returns the last item in the list. 
+    # (The square brackets around the i in the method signature denote that the parameter is optional, 
+    # not that you should type square brackets at that position. 
+    # You will see this notation frequently in the Python Library Reference.)
+    def pop(self, index = None):
+        pass
+    
+    # Remove all items from the linked list.
+    def clear(self):
+        self._head = None
+        self._tail = None
+        self._len = 0
+        
+    # Return zero-based index in the list of the first item whose value is equal to x. 
+    # Raises a ValueError if there is no such item.
+    # The optional arguments start and end are interpreted as in the slice notation and are used to limit the search to a particular subsequence of the list. 
+    # The returned index is computed relative to the beginning of the full sequence rather than the start argument.
+    def index(self, data, start = None, end = None):
+        pass
+    #     if start == None:
+    #         start = 0
+    #     if end == None:
+    #         end = self._count
+    #     node = self._head
+    #     for _ in range(start, end):
+    #         if node.data == data:
+    #             return _
+    #         node = node.next
+    #     raise ValueError(f"{data} is not in linked list."
+    
+    # Return the number of times x appears in the list.
+    def count(self, data):
+        pass
+
+# Create data for sort
+def create_random_data(size:int = 10000) -> LinkedList:
+    linked_list = LinkedList()
+    return [random.randint() for _ in range(size)]
 
 
 # define main function
